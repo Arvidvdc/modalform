581a558331a0c752ea6851bab2bd9e473f664d71
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-04 09:53:20 +0100

    Added comments and updated english.

--- js/main.js
+++ js/main.js
@@ -7,28 +7,28 @@ $(document).ready(function() {
         $('#staticBackdrop').modal('show');
     }
 });
-
+// When loading page, fill day- and year dropdownlists
 $(window).on('load',function(){
     filDayCombo();
     filYearCombo();
 });
 
-//Change sessionStorage:ageVerified to true // sessionStorage wil be cleared after browser restarts
+// Change sessionStorage:ageVerified to true // sessionStorage wil be cleared after browser restarts
 $(document).on("click", "#buttonAgeCheck", function(){
     let buttonContinue = document.getElementById("buttonContinue");
-    // opbouwen opgegeven leeftijd
+    // Build age variable
     let enterdAge = getAge(document.getElementById("ageYear").value +"/" + document.getElementById("ageMonth").value +"/" + document.getElementById("ageDay").value);
     let minimumAge = 18;
-
     if(enterdAge<minimumAge) {
-        buttonContinue.innerText="Sorry to young";
+        buttonContinue.innerText="Sorry too young";
     } else {
         ageVerified = true;
         sessionStorage.setItem("ageVerified", JSON.stringify(ageVerified));
-        buttonContinue.innerText="Welkom";
+        buttonContinue.innerText="Welcome";
     }
 });
 
+// Button on the page to call the Modal form
 $(document).on("click", "#buttonModal", function(){
     let buttonContinue = document.getElementById("buttonContinue");
     ageVerified = false;
@@ -40,6 +40,7 @@ $(document).on("click", "#buttonModal", function(){
     $("#ageYear").val(currentYear());
 });
 
+// Button to continue (when age is approved) of to leave (goto google.com)
 $(document).on("click", "#buttonContinue", function(){
     let ageVerified = JSON.parse(sessionStorage.getItem("ageVerified"));
     if(!ageVerified){
@@ -62,7 +63,13 @@ function getAge(dateString) {
 }
 // console.log('age: ' + getAge("2004/07/05"));
 
-//Function for filling dropdown elements
+// Function for getting current year
+function currentYear() {
+    let vandaag = new Date()
+    return vandaag.getFullYear();
+}
+
+//Function for filling dropdown element ageDay
 function filDayCombo(){
     $('#ageDay').empty()
     for (let i = 1; i < 32; i++) {
@@ -78,11 +85,9 @@ function filDayCombo(){
     }
 }
 
-function currentYear() {
-    let vandaag = new Date()
-    return vandaag.getFullYear();
-}
+// Element ageMonth will be filled in the HTML-scource 
 
+//Function for filling dropdown element ageYear
 function filYearCombo(){
     $('#ageYear').empty()
     let baseYear =currentYear();
